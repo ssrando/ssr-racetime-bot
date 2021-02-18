3713db22bc8642d69a9ea704f60f63bf9d7dbb13
Floha <floha2011@hotmail.de>
2021-02-18 17:43:31 +0100

    update submodule

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     },
     version='1.0.0',
     install_requires=[
-        'racetime_bot@git+git://github.com/racetimeGG/racetime-bot@tww-rando-bot',
+        'racetime_bot@git+git://github.com/wooferzfg/racetime-bot@tww-rando-bot',
         'PyGithub>=1.53'
     ],
     packages=find_packages(),
