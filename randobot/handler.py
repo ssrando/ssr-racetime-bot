f7bf9216061af84ad28887ab97a38da2348b72aa
CelestialPanda- <30158363+Celestial-Panda@users.noreply.github.com>
2022-08-31 02:44:31 +0200

    Translated most of the messages to French

--- randobot/handler.py
+++ randobot/handler.py
@@ -60,8 +60,12 @@ class RandoHandler(RaceHandler):
         self.state["spoiler"] = spoiler
         if spoiler:
             await self.send_message("Will create a public sharable Spoiler Log")
+            if self.state.get("use_french"):
+                await self.send_message("Un Spoiler Log public et partageable sera créé")
         else:
             await self.send_message("Will NOT create a public sharable Spoiler Log")
+            if self.state.get("use_french"):
+                await self.send_message("Un Spoiler Log public et partageable ne sera PAS crée")
 
     async def ex_info(self, args, message):
         response = ""
@@ -103,7 +107,7 @@ class RandoHandler(RaceHandler):
         await self.send_message("Seed rolling is now locked.")
         if self.state.get("use_french"):
             await self.send_message(
-                "Translate 'Seed rolling is now locked.' to French."
+                "La génération de seed est désormais bloquée."
             )
 
     @monitor_cmd
@@ -111,7 +115,7 @@ class RandoHandler(RaceHandler):
         self.state["locked"] = False
         await self.send_message("Seed rolling is now unlocked")
         if self.state.get("use_french"):
-            await self.send_message("Translate 'Seed rolling is now locked' to French.")
+            await self.send_message("La génération de seed est désormais débloquée.")
 
     @monitor_cmd
     async def ex_reset(self, args, message):
@@ -124,71 +128,107 @@ class RandoHandler(RaceHandler):
         self.state["version"] = None
         self.state["draft"] = None
         await self.send_message("The Seed has been reset.")
+        if self.state.get("use_french"):
+            await self.send_message("La Seed a été réinitialisée")
 
     async def ex_permalink(self, args, message):
         permalink = args[0]
         self.state["permalink"] = permalink
         await self.send_message(f"Updated permalink to {permalink}")
+        if self.state.get("use_french"):
+            await self.send_message(f"Permalien mis à jour: {permalink}")
 
     async def ex_sgl(self, args, message):
         self.state["permalink"] = "IQ0IIDsD85rpUwAAAAAAACHIFwA="
         await self.send_message(f"Updated the bot to SGL settings")
+        if self.state.get("use_french"):
+            await self.send_message("Mis à jour le bot pour les paramètres SGL")
 
     async def ex_coop(self, args, message):
         self.state["permalink"] = "oQ0AIBAD85oJUgAAAAAAAAAQAw=="
         self.state["version"] = "1.2.0_3868e57"
         await self.send_message("Updated the bot to Co-Op S1 settings")
-    
+        if self.state.get("use_french"):
+            await self.send_message("Mis à jour le bot pour les paramètres Co-Op S1")
+
     async def ex_s2(self, args, message):
         self.state["version"] = "1.2.0_f268afa"
         self.state["draft"] = Draft()
         self.state["draft"].set_log_state("off")
-        await self.send_message("Updated the bot to Season 2 version. Draft mode has been enabled and reset, and the spoiler log has been disabled. You may now use the command !draftguide (high seed) (low seed) to guide you through the draft process with two players.")
+        await self.send_message(
+            "Updated the bot to Season 2 version. Draft mode has been enabled and reset, and the spoiler log has been disabled. You may now use the command !draftguide (high seed) (low seed) to guide you through the draft process with two players.")
+        if self.state.get("use_french"):
+            await self.send_message(
+                "Mis à jour le bot à la version Saison 2. 'Draft Mode' a été activé et réinitialisé, et le spoiler log a été désactivé. Vous pouvez maintenant utiliser la commande !draftguide (seed haute) (seed basse) pour vous guider durant le processus de sélection avec deux joueurs.")
 
     async def ex_version(self, args, message):
         version = args[0]
         self.state["version"] = version
         await self.send_message(f"Version set to {version}")
+        if self.state.get("use_french"):
+            await self.send_message(f"Version définie à {version}")
 
     async def ex_draft(self, args, message):
         if self.state["draft"] is not None:
             await self.send_message("Draft mode is already active")
+            if self.state.get("use_french"):
+                await self.send_message("'Draft Mode' est déjà actif")
         else:
             self.state["draft"] = Draft()
             await self.send_message(
                 "Draft mode activated. The !ban and !pick commands are now active"
             )
+            if self.state.get("use_french"):
+                await self.send_message(
+                    "'Draft Mode' activé. Les commandes !ban et !pick sont désormais utilisables"
+                )
 
     async def ex_draftoff(self, args, message):
         self.state["draft"] = None
         await self.send_message("Draft mode deactivated")
+        if self.state.get("use_french"):
+            await self.send_message("'Draft Mode' désactivé")
 
     async def ex_ban(self, args, message):
         if self.state["draft"] is None:
             await self.send_message("Draft mode is not active")
+            if self.state.get("use_french"):
+                await self.send_message("Draft Mode' n'est pas actif")
         else:
             if len(args) == 0:
                 await self.send_message("No mode specified")
+                if self.state.get("use_french"):
+                    await self.send_message("Aucun mode spécifié")
             else:
                 await self.send_message(self.state["draft"].ban(" ".join(args)))
 
     async def ex_pick(self, args, message):
         if self.state["draft"] is None:
             await self.send_message("Draft mode is not active")
+            if self.state.get("use_french"):
+                await self.send_message("'Draft Mode' n'est pas actif")
         else:
             if len(args) == 0:
                 await self.send_message("No mode specified")
+                if self.state.get("use_french"):
+                    await self.send_message("Aucun mode spécifié")
             else:
                 await self.send_message(self.state["draft"].pick(" ".join(args)))
 
     async def ex_draftlog(self, args, message):
         if self.state["draft"] is None:
             await self.send_message("Draft mode is not active")
+            if self.state.get("use_french"):
+                await self.send_message("'Draft Mode' n'est pas actif")
         else:
             if len(args) == 0:
                 await self.send_message(
                     "Please specify 'off' or 'on' to deactivate or activate the randomizer's spoiler log generation."
                 )
+                if self.state.get("use_french"):
+                    await self.send_message(
+                        "Veuillez spécifier 'off' ou 'on' pour désactiver ou activer la génération du Spoier Log"
+                    )
             else:
                 await self.send_message(
                     self.state["draft"].set_log_state("".join(args).strip())
@@ -197,11 +237,17 @@ class RandoHandler(RaceHandler):
     async def ex_draftguide(self, args, message):
         if self.state["draft"] is None:
             await self.send_message("Draft mode is not active")
+            if self.state.get("use_french"):
+                await self.send_message("'Draft Mode' n'est pas actif")
         else:
             if len(args) != 2:
                 await self.send_message(
                     "Please specify the higher seed and lower seed player names (in 1 word each) respectively for the guide process."
                 )
+                if self.state.get("use_french"):
+                    await self.send_message(
+                        "Veuillez spécifier les noms de la seed la plus haute et plus basse (en 1 mot chacun) pour le guidage."
+                    )
             else:
                 self.state["draft"].banned = []
                 self.state["draft"].picked = []
@@ -212,13 +258,19 @@ class RandoHandler(RaceHandler):
     async def ex_draftguideoff(self, args, message):
         if self.state["draft"] is None:
             await self.send_message("Draft mode is not active")
+            if self.state.get("use_french"):
+                await self.send_message("'Draft Mode' n'est pas actif")
         self.state["draft"].guide_step = None
         await self.send_message("Draft guide mode deactivated.")
+        if self.state.get("use_french"):
+            await self.send_message("Guide du 'Draft Mode' désactivé")
 
     async def ex_draftstatus(self, args, message):
         draft = self.state["draft"]
         if draft is None:
             await self.send_message("Draft mode is not active")
+            if self.state.get("use_french"):
+                await self.send_message("'Draft Mode' n'est pas actif")
         else:
             status_message = f"Draft mode is active. Currently banned: {draft.banned}. Currently picked: {draft.picked}. Spoiler log: {draft.spoiler_log}."
             if self.state["draft"].guide_step is not None:
@@ -235,6 +287,8 @@ class RandoHandler(RaceHandler):
     async def ex_draftoptions(self, args, message):
         if self.state["draft"] is None:
             await self.send_message("Draft mode is not active")
+            if self.state.get("use_french"):
+                await self.send_message("'Draft Mode' n'est pas actif")
         else:
             await self.send_message(
                 f"Draft options: {', '.join(self.state['draft'].OPTIONS.keys())}"
@@ -249,16 +303,17 @@ class RandoHandler(RaceHandler):
             )
             if self.state.get("use_french"):
                 await self.send_message(
-                    "Translate 'Seed rolling is locked! Only the creator of this room, a race "
-                    "monitor, or a moderator can roll a seed.'"
+                    "La génération de seed est bloquée! Seul le créateur de la salle, un moniteur, "
+                    "ou un modérateur peut générer une seed."
                 )
+
             return
 
         if self.state.get("permalink_available"):
             await self.send_message("The seed is already rolled! Use !seed to view it.")
             if self.state.get("use_french"):
                 await self.send_message(
-                    "Translate 'The seed is already rolled! Use !permalink to view it.' to French."
+                    "La seed a déjà été générée! Utilisez !seed pour la voir."
                 )
             return
 
@@ -287,7 +342,7 @@ class RandoHandler(RaceHandler):
             current_hash.update(permalink.encode("ASCII"))
             current_hash.update(version.encode("ASCII"))
             with urllib.request.urlopen(
-                f"http://raw.githubusercontent.com/ssrando/ssrando/{commit}/names.txt"
+                    f"http://raw.githubusercontent.com/ssrando/ssrando/{commit}/names.txt"
             ) as f:
                 data = f.read().decode("utf-8")
                 names = [s.strip() for s in data.split("\n")]
@@ -309,6 +364,8 @@ class RandoHandler(RaceHandler):
             url = generated_seed.get("spoiler_log_url")
             self.state["spoiler_url"] = url
             await self.send_message(f"Spoiler Log URL available at {url}")
+            if self.state.get("use_french"):
+                await self.send_message(f"Spoiler Log disponible à l'url: {url}")
 
         if self.state["draft"] is not None:
             await self.set_raceinfo(
